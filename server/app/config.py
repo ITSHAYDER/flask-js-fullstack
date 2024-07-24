36de36fc739e7aa629ffc906e50d69b86533075a
its_hayder <87187471+ITSHAYDER@users.noreply.github.com>
2024-07-24 23:25:21 +0100

    Update config.py

--- server/app/config.py
+++ server/app/config.py
@@ -9,7 +9,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
     SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
-
-    API_USERS = {
-    "hayder": "this"
-    }
